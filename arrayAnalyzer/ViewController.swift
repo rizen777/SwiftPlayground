46619af9bf272b8b18120adf8f904716ac3befee
Admin <admin@mac-admin.local>
2016-05-04 14:20:32 +0300

    initial commit

--- arrayAnalyzer/ViewController.swift
+++ arrayAnalyzer/ViewController.swift
@@ -12,14 +12,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let array1 = [1, 2, 3, 4, 5]
+        let array2 = [2, 9, -1000, 3, 1, 0, 5]
+        let result = analyzeArray(array1, array2: array2)
+        print("UniqueInFirst \(result.0) UniqueInSecond \(result.1) CommonItems \(result.2)")
     }
-
+	
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func analyzeArray(array1 : [Int], array2 : [Int]) -> (Set<Int>, Set<Int>, Set<Int>){
+        var uniqueInFirstArray = Set(array1)
+        var uniqueInSecondArray = Set<Int>()
+        var commonItems = Set<Int>()
+        for item in array2 {
+            if uniqueInFirstArray.contains(item){
+                commonItems.insert(item)
+                uniqueInFirstArray.remove(item)
+            } else{
+                uniqueInSecondArray.insert(item)
+            }
+        }
+        return (uniqueInFirstArray, uniqueInSecondArray, commonItems)
+    }
 }
 
